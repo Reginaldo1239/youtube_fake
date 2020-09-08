d8db872c82474cdfa505e30e75448dea60cc1f2a
reginaldo melo <reginaldo.melo56@gmail.com>
2020-09-08 16:06:36 -0300

    alteei o redux

--- src/redux/redutors/youtube/index.js
+++ src/redux/redutors/youtube/index.js
@@ -1,11 +1,12 @@
 
 const stateInitial = {nextPageToken:null,prevPageToken:null,listVideos:[],listVideosWatch:[]}
 export default function YoutubeReducer (state=stateInitial,action){
+    console.log(state)
     switch(action.type){
         case 'ADD_VIDEO_TO_LIST':
             return {
                 ...state,listVideos:state.listVideos.concat(action.newVideos),
-                nextPageToken:state.nextPageToken,
+                nextPageToken:action.nextPageToken,
                 prevPageToken:action.prevPageToken
             }
         case 'RESTART_VIDEO_LIST':
