708033c02ffe0fab31c2266ef39c32f845f134e2
reginaldo melo <reginaldo.melo56@gmail.com>
2020-09-08 14:35:48 -0300

    Updates

---
+++ precache-manifest.0e439e619296c52c4b1091a8b81baa71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89834144cfac110d9dad6a136f979fa4",
+    "url": "/youtube_fake/index.html"
+  },
+  {
+    "revision": "a88f56079348c2c6e2ac",
+    "url": "/youtube_fake/static/css/main.0db4f7f6.chunk.css"
+  },
+  {
+    "revision": "6045c5b620c1c2bce812",
+    "url": "/youtube_fake/static/js/2.c9518708.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/youtube_fake/static/js/2.c9518708.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a88f56079348c2c6e2ac",
+    "url": "/youtube_fake/static/js/main.03323d36.chunk.js"
+  },
+  {
+    "revision": "b43772066ad9deea901a",
+    "url": "/youtube_fake/static/js/runtime-main.4fa0eade.js"
+  }
+]);
