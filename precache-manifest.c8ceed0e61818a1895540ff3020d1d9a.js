f9312216e2396b72e43ec2fea5b4064ec756b22e
reginaldo melo <reginaldo.melo56@gmail.com>
2020-09-08 16:05:23 -0300

    Updates

--- precache-manifest.c8ceed0e61818a1895540ff3020d1d9a.js
+++ precache-manifest.c8ceed0e61818a1895540ff3020d1d9a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b82fadf2b8d90d0c8c5c6e7f94ca481",
+    "revision": "d9458b46ce981049e8e5f13811dd01a6",
     "url": "/youtube_fake/index.html"
   },
   {
-    "revision": "f9c9e78a35540b11f44b",
+    "revision": "5bebeee87d1e23d6a8be",
     "url": "/youtube_fake/static/css/main.ed8ff8d1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/youtube_fake/static/js/2.c9518708.chunk.js.LICENSE"
   },
   {
-    "revision": "f9c9e78a35540b11f44b",
-    "url": "/youtube_fake/static/js/main.57a09955.chunk.js"
+    "revision": "5bebeee87d1e23d6a8be",
+    "url": "/youtube_fake/static/js/main.f9a81e39.chunk.js"
   },
   {
     "revision": "b43772066ad9deea901a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youtube_fake/precache-manifest.53335ff05a92e368e4c5fc48ff57c887.js"
+  "/youtube_fake/precache-manifest.c8ceed0e61818a1895540ff3020d1d9a.js"
 );
 
 self.addEventListener('message', (event) => {
